0eda5e33277b19ff96470d11650d9948c3eb4bb0
alvigabriel <claudio.alves20112011@hotmail.com>
2023-04-26 11:40:52 -0300

    Iniciando com dotnet

---
+++ Aula mostrando dotnet/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite os números a serem somados");
+
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine( $"O resultado da soma é {Soma(numero1, numero2)}" );
+
+            Console.WriteLine("Aperte enter para finalizar o programa");
+
+            Console.ReadLine();
+        }
+
+        public static int Soma(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+}
