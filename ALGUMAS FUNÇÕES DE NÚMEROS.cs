59a88ff5c4b378099f057b27355b6d82cbe65bbc
alvigabriel <claudio.alves20112011@hotmail.com>
2023-04-28 12:23:59 -0300

    ALGUMAS FUNÇÕES DE NÚMEROS

---
+++ ALGUMAS FUNÇÕES DE NÚMEROS.cs
@@ -0,0 +1,15 @@
+tipos numéricos
+
+int a = 10;
+float b = 9.999;
+
+
+int max = int.MaxValue;
+int min = int.MinValue;
+
+Console.WriteLine($"O limite máximo da variável Integer é de {min} até {max}");
+
+double maxD = double.MaxValue;
+double minD = double.MinValue;
+
+Console.WriteLine($"O limite máximo da variável Double é de {minD} até {maxD}");
