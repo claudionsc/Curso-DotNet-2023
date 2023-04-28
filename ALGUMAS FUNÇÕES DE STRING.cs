28ffe141ef39656b6f614664648b8b9155661ec2
alvigabriel <claudio.alves20112011@hotmail.com>
2023-04-28 11:44:26 -0300

    ALGUMAS FUNÇÕES DE STRING

---
+++ ALGUMAS FUNÇÕES DE STRING.cs
@@ -0,0 +1,17 @@
+// ALGUMAS FUNÇÕES DO C#
+
+string saudacao = "          Olá Mundo!            ";
+Console.WriteLine($"[{saudacao}]");
+
+// REMOVE ESPAÇOES EM BRANCO DO COMEÇO
+string apararSaudacao = saudacao.TrimStart();
+Console.WriteLine($"[{apararSaudacao}]");
+
+// REMOVE ESPAÇOES EM BRANCO DO FINAL
+apararSaudacao = saudacao.TrimEnd();
+Console.WriteLine($"[{apararSaudacao}]");
+
+// REMOVE TODOS OS ESPAÇOES EM BRANCO 
+apararSaudacao = saudacao.Trim();
+Console.WriteLine($"[{apararSaudacao}]");
+
