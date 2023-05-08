816f6ab17ea98d4264fec43644ba26e3d0663d78
alvigabriel <claudio.alves20112011@hotmail.com>
2023-05-08 08:47:33 -0300

    Override

--- Aula 03/ConsoleApp1/Program.cs
+++ Aula 03/ConsoleApp1/Program.cs
@@ -11,21 +11,22 @@ namespace ConsoleApp1
             //coleção List
             Console.WriteLine("Hello, World");
 
-            Carro carro1 = new Carro();
+            Carro carro1 = new Carro("Carro 1 ========");
             
             Carro carro2 = new Carro("Sou o carro 2");
 
-            Carro carro3 = new Carro();
+            Carro carro3 = new Carro("Carro 3 ----------");
 
-            Console.WriteLine(carro1.DigaSeuNome("Carro 1"));
-            Console.WriteLine(carro2.DigaSeuNome());
-            Console.WriteLine(carro2.DataRegistro());
-            Console.WriteLine(carro3.DigaSeuNome("Carro 3"));
+            Console.WriteLine(carro1);
+            Console.WriteLine(carro2);
+            Console.WriteLine(carro3);
+
+            //Console.WriteLine(carro1.DigaSeuNome("Carro 1"));
+            //Console.WriteLine(carro2.DigaSeuNome());
+            //Console.WriteLine(carro2.DataRegistro());
+            //Console.WriteLine(carro3.DigaSeuNome("Carro 3"));
 
             Console.Read();
-
-
-
 
         }
 
@@ -43,6 +44,14 @@ namespace ConsoleApp1
             public Carro()
             {
                 
+            }
+
+
+            //override serve para todas as classes
+
+            public override string ToString()
+            {
+                return string.Format("Eu sou o carro {0} e fui registrado em {1}", this.nome, this.DataRegistro());
             }
 
             //sobrecarga de método
