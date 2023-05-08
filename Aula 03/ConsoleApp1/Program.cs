5b27900369d316c3c3f55e3200d7800d1641822b
alvigabriel <claudio.alves20112011@hotmail.com>
2023-05-08 08:19:34 -0300

    Sobrecarga de métodos

--- Aula 03/ConsoleApp1/Program.cs
+++ Aula 03/ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.CompilerServices;
+
 namespace ConsoleApp1
 {
     class Program
@@ -11,12 +13,13 @@ namespace ConsoleApp1
 
             Carro carro1 = new Carro();
             
-            Carro carro2 = new Carro();
+            Carro carro2 = new Carro("Sou o carro 2");
 
             Carro carro3 = new Carro();
 
             Console.WriteLine(carro1.DigaSeuNome("Carro 1"));
-            Console.WriteLine(carro2.DigaSeuNome("Carro 2"));
+            Console.WriteLine(carro2.DigaSeuNome());
+            Console.WriteLine(carro2.DataRegistro());
             Console.WriteLine(carro3.DigaSeuNome("Carro 3"));
 
             Console.Read();
@@ -28,9 +31,33 @@ namespace ConsoleApp1
 
         class Carro
         {
+            //construtor, um método que é chamado sempre que a classe
+            //é instanciada
+
+            private string nome;
+            public Carro(string Nome)
+            {
+                nome = Nome;
+            }
+
+            public Carro()
+            {
+                
+            }
+
+            //sobrecarga de método
+            public string DigaSeuNome()
+            {
+                return nome;
+            }
             public string DigaSeuNome(string Nome) 
             {
                 return Nome;
+            }
+
+            public DateTime DataRegistro()
+            {
+                return DateTime.Now;
             }
         }
     }
