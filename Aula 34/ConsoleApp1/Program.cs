db25314a62088c141261f2be39eff8989ea4266d
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-05-09 09:34:16 -0300

    List, IndexOf

---
+++ Aula 34/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nomes = new List<string> { "<nome>", "Ana", "Felipe " };
+
+            nomes.Add("Diogo");
+            nomes.Add("Maria");
+            nomes.Add("Ana");
+
+            foreach (var nome in nomes)
+            {
+                Console.WriteLine($"Olá {nome.ToUpper()}");
+            }
+            var index = nomes.IndexOf("Diogo");
+            Console.WriteLine($"Encontrei o Diogo na posição {index} ");
+        }
+    }
+}
