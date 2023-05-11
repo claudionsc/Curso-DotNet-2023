ede9f3059fe75aa5efb343f84ff9ae42ee834746
alvigabriel <claudio.alves20112011@hotmail.com>
2023-05-11 09:30:13 -0300

    modificador static, evoluindo aplicação do banco, criando método depositar

--- POO/MeuSuperBanco/ContaBanco.cs
+++ POO/MeuSuperBanco/ContaBanco.cs
@@ -10,12 +10,48 @@ namespace MeuSuperBanco
     {
         public string Numero { get; }
         public string Dono { get; set; }
-        public decimal Saldo { get; }
+        public decimal Saldo 
+        { 
+            get
+            {
+                decimal saldo = 0;
+                foreach (var item in todasTransacoes) 
+                {
+                    saldo += item.Valor;
+                }
+                return saldo;
+            }
+        }
+
+        /* static é um modificador de variáveis.
+         uma variável estática não depende da instanciação
+        de uma classe pra ser acessada, pois a mesma pertence 
+         à classe e não à sua instância */
+        public static int numeroConta = 1234567890;
+
+        private List<Transacao> todasTransacoes = new List<Transacao>();
 
         public ContaBanco(string nome, decimal valor)
         {
             this.Dono = nome;
-            this.Saldo = valor;
+
+            numeroConta++;
+
+            this.Numero = numeroConta.ToString();
+            Depositar(valor, DateTime.Now, "Saldo Inicial");
         }
+
+
+    public void Depositar(decimal valor, DateTime data, string obs)
+    {
+            Transacao trans = new Transacao(valor, data, obs);
+            todasTransacoes.Add(trans);
+    }
+
+    public void Sacar()
+    {
+
+    }
+
     }
 }
