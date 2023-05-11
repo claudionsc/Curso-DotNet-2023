49b8d72ca8233678471fe5197af8e1490d7ddea3
alvigabriel <claudio.alves20112011@hotmail.com>
2023-05-11 08:34:20 -0300

    Iniciando POO, Meu super banco

---
+++ POO/MeuSuperBanco/ContaBanco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuSuperBanco
+{
+    public class ContaBanco
+    {
+        public string Numero { get; }
+        public string Dono { get; set; }
+        public decimal Saldo { get; }
+
+        public ContaBanco(string nome, decimal valor)
+        {
+            this.Dono = nome;
+            this.Saldo = valor;
+        }
+    }
+}
---
+++ POO/MeuSuperBanco/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MeuSuperBanco;
+
+Console.WriteLine("Hello, World!");
+
+ContaBanco contaB = new ContaBanco("Diogo", 1000);
+Console.WriteLine($"A conta {contaB.Numero} de {contaB.Dono} foi criada com o saldo de {contaB.Saldo} reais ");
