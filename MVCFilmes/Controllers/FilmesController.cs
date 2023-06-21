0a827321a7503f230924077a5781aefb511e2d97
alvigabriel <claudio.alves20112011@hotmail.com>
2023-06-21 11:16:29 -0300

    MVC Filmes - Controller

---
+++ MVCFilmes/Controllers/FilmesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCFilmes.Controllers
+{
+    public class FilmesController : Controller
+    {
+        public string Index() => "Essa é mimha action padrão";
+    }
+
+    public string BemVindo() => "Olá, seja muito bem vindo"
+}
