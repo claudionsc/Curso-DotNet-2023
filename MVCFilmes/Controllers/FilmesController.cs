ee01ac1c8f0871b943e10978a7fc88368e7d45af
alvigabriel <claudio.alves20112011@hotmail.com>
2023-06-21 11:33:50 -0300

    Adicionando parametros às rotas

--- MVCFilmes/Controllers/FilmesController.cs
+++ MVCFilmes/Controllers/FilmesController.cs
@@ -5,7 +5,7 @@ namespace MVCFilmes.Controllers
     public class FilmesController : Controller
     {
         public string Index() => "Essa é mimha action padrão";
+        public string BemVindo(string nome) => $"Olá, seja muito bem vindo {nome}";
     }
 
-    public string BemVindo() => "Olá, seja muito bem vindo"
 }
