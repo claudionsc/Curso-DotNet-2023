10fd6222dcb653e2e89fc653d305f3939ebecc6c
alvigabriel <claudio.alves20112011@hotmail.com>
2023-06-28 11:26:24 -0300

    Data Annotations 01

--- MVCFilmes/Models/Filmes.cs
+++ MVCFilmes/Models/Filmes.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVCFilmes.Models
 {
@@ -6,9 +7,16 @@ namespace MVCFilmes.Models
     {
         public int ID { get; set; }
         public string Titulo { get; set; } = string.Empty;
+
+        [Display(Name = "Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTime dataLancamento { get; set; }
+
+        [Display(Name = "Gênero")]
         public string Genero { get; set; } = string.Empty;
+
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(18, 2")]
         public decimal Preco { get; set; }
     }
 }
