fcbb2f3760e843de35573f1a5ded1ff41a8dd5cb
alvigabriel <claudio.alves20112011@hotmail.com>
2023-07-01 11:55:24 -0300

    Validações de campos

--- MVCFilmes/Models/Filmes.cs
+++ MVCFilmes/Models/Filmes.cs
@@ -6,19 +6,32 @@ namespace MVCFilmes.Models
     public class Filmes
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [MaxLength(100)]
         public string Titulo { get; set; } = string.Empty;
 
         [Display(Name = "Data de Lançamento")]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Campo obrigatório")]
         public DateTime dataLancamento { get; set; }
 
         [Display(Name = "Gênero")]
+        [
+            Required(ErrorMessage = "Campo obrigatório"), 
+            StringLength(10),
+            RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "Apenas letras são permitidas")
+            ]
+
         public string Genero { get; set; } = string.Empty;
 
         [Display(Name = "Preço")]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
+        [Required(ErrorMessage = "Campo obrigatório")]
         public decimal Preco { get; set; }
 
-       public int? Pontos { get; set; }
+        [Range(0,5)]
+        public int? Pontos { get; set; }
     }
 }
---
+++ MVCFilmes/wwwroot/js/jsHelperPtBR.js
@@ -0,0 +1,3 @@
+﻿$.validator.methods.number = function (value, number) {
+    return this.optional(element) || /^(?:-?\d+|-?\d{1,3}(?:.\d{3})+)?(?:\,\d+)?$/.test(value);
+}
