2d6ae47331f96bbe720b76ee6a593d0c352914b9
alvigabriel <claudio.alves20112011@hotmail.com>
2023-07-07 10:34:05 -0300

    DBSet

--- Swagger-App01/Contexts/TarefaContexto.cs
+++ Swagger-App01/Contexts/TarefaContexto.cs
@@ -1,6 +1,15 @@
-﻿namespace Swagger_App01.Contexts
+﻿using Microsoft.EntityFrameworkCore;
+using Swagger_App01.Model;
+
+namespace Swagger_App01.Contexts
 {
-    public class TarefaContexto
+    public class TarefaContexto : DbContext
     {
+        public TarefaContexto(DbContextOptions<TarefaContexto> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<TarefaItem> TarefaItems { get; set; } = null; // liga a classe criada no modelo e liga com o contexto
     }
 }
