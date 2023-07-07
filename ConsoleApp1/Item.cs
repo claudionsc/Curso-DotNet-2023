b8c6beaa9f23aea29608adc9d615296ef520cfae
alvigabriel <claudio.alves20112011@hotmail.com>
2023-07-07 12:16:26 -0300

    Adicionando um novo projeto na solução, windows forms, console, flurl

---
+++ ConsoleApp1/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Item
+    {
+        public long ID { get; set; }
+        public string? Nome { get; set; }
+        public bool Finalizado { get; set; }
+    }
+}
---
+++ Consumidor.API/Trabalho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumidor.API
+{
+    public class Trabalho
+    {
+        public long ID { get; set; }
+        public string? Nome { get; set; }
+        public bool Finalizado { get; set; }
+    }
+}
