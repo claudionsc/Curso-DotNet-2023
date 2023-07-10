07777b9e220538f41e9b4fc275d63af40de68110
alvigabriel <claudio.alves20112011@hotmail.com>
2023-07-10 11:51:20 -0300

    teste01

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp1;
+using Flurl;
+using Flurl.Http;
+using System.Runtime.Versioning;
 
 Console.WriteLine("Hello, World!");
 
@@ -17,7 +20,15 @@ tarefa2.Nome = "Lavar louças";
 tarefa2.Finalizado = false;
 
 
+string endpoint = url + "api/TarefaItems";
 
+// retardar a chamada e esperar a nossa api estar no ar
+Thread.Sleep(new TimeSpan(0, 0, 5));
+
+
+//flurl -  Funciona mais ou menos como Axios
+endpoint.PostJsonAsync(tarefa1);
+endpoint.PostJsonAsync(tarefa2);
 
 Console.WriteLine("Aperte qualquer tecla para finalizar a aplicação!");
 
