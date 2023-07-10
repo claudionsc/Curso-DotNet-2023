85e51b8915bfc50fc0d11ec6cb69d149c63ca6c5
alvigabriel <claudio.alves20112011@hotmail.com>
2023-07-10 11:24:54 -0300

    Instaciando classe tarefa, inserindo endpoint para conexão com api

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
+
+
+string url = "https://localhost:7035/";
+
+Item tarefa1 = new Item();
+tarefa1.ID = 1;
+tarefa1.Nome = "Pagar a conta";
+tarefa1.Finalizado = true;
+
+Item tarefa2 = new Item();
+tarefa2.ID = 1;
+tarefa2.Nome = "Lavar louças";
+tarefa2.Finalizado = false;
+
+
+
+
+Console.WriteLine("Aperte qualquer tecla para finalizar a aplicação!");
+
+Console.Read();
